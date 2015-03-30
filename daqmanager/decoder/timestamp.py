328390cbc8ca343c0c4a24c1ad9555f201f5ab1f
yshao <yshaoeng@gmail.com>
2015-03-30 11:53:30 -0600

    update

---
+++ daqmanager/decoder/timestamp.py
@@ -0,0 +1,27 @@
+###
+
+aDiff=aTime[np.where()]
+
+aCS=np.array(range(0.255))
+
+aCSCounter=np.vstack(aCS)
+
+### slide timestamp counter into aCS counter
+for i in aDiff:
+  st=aDiff[i]
+  end=aDiff[i+1]
+  aCSSub=aDiff[st:end]
+  for 
+
+aCSCounter=np.vstack(aCSCounter,aCSSub)
+ 
+
+###
+
+indices = np.arange(features.shape[0])
+feature_indices = F(l)
+
+features.flat[indices, feature_indices] = 1
+
+
+Diagnosis
